fe4067a3b145cd4a3ded81a774c0938ee987b1be
Sagarika Nanda <snand2009@gmail.com>
2020-07-30 21:40:03 -0400

    added validation

--- script.js
+++ script.js
@@ -1,91 +1,77 @@
 
-/*var today = new Date();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var dateTime = date+' '+time;
-document.getElementById("p1").innerHTML = date;
-document.getElementById("p2").innerHTML = time;*/
 
 
-var today = new Date();
-var date = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
-var time = today.getHours() + ":"+ today.getMinutes() + ":" +today.getSeconds();
-var date = (today.getMonth()+1)+'-'+today.getDate()+'-'+today.getFullYear();
-var time = today.getHours() + ":"+ today.getMinutes() + ":" +today.getSeconds();
-var dateTime = date+' '+time;
-
-document.getElementById("p1").innerHTML = Date();
-/*document.getElementById("p2").innerHTML = current();*/
+/*Javascript function to show current date and time in interval of seconds*/
 
 
-
-
-
-
-
-
-function validateForm1()
+function showTime()
  {
-  var x = document.forms["myForm1"]["fname"].value;
-  var y = document.forms["myForm1"]["lname"].value;
-  if (x == "" && y == "")
-  {
-    alert("first name and last name must be filled out");
-    return false;
-  } 
+  var d = new Date();  
   
-     document.write("your first name is:"+" "+ x. fontsize(10)+"and the number of alphabets are:"+" "+ x.length+" ");
-     
-    document.write("your last name is:"+" "+ y.fontsize(10) +" "+"and the number of alphabets are:"+" "+y.length);
-    
-    document.write("the concatention of first name and last name is: "+" "+ x.fontsize(10) +" "+ y. fontsize(10));
+  document.getElementById("p1").innerHTML = d.toLocaleDateString();
+  document.getElementById("p2").innerHTML = d.toLocaleTimeString();  
+}
+setInterval(showTime, 1000);
 
 
+
+/* Validation of form in contact4.html*/
+
+
+
+  function validate()
+   {
+      
+    if( document.myForm.Name.value == "" ) 
+    {
+       alert( "Please provide your name!" );
+       document.myForm.Name.focus() ;
+       return false;
+    }
+
+  var emailID = document.myForm.EMail.value;
+  x = emailID.indexOf("@");
+  y = emailID.lastIndexOf(".");
+  
+  if (x < 1 || ( y - x < 2 ))
+   {
+     alert("Please enter correct email ID")
+     document.myForm.EMail.focus() ;
+     return false;
   }
+  
+ 
 
-
-/*function validateForm2() {
-  var y = document.forms["myForm2"]["lname"].value;
-  if (y == "") {
-    alert("last Name must be filled out");
-    return false;
-  } else
-    document.write(y);
+    if( document.myForm.Zip.value == "" || isNaN( document.myForm.Zip.value ) ||
+       document.myForm.Zip.value.length != 5 ) 
+       {
+       
+       alert( "Please provide a zip in the format #####." );
+       document.myForm.Zip.focus() ;
+       return false;
+    }
+    if( document.myForm.Country.value == "-1" )
+     {
+       alert( "Please provide your country!" );
+       return false;
+    }
+    return( true );
+ 
   }
-z = (x.length) + (y. length);
-document.write(z);*/
+ 
 
 
+/*validation of user can't leave blank
 
   function email_function() {
-    var email = document.forms["emailform"]["email"].value;
+    var email = document.forms["myform"]["Email"].value;
     if (email == "") {
       alert("Insert your Email");
       return false;
     }
-  }
+  }*/
 
-  /*function email_funnction() {
-    var email = document.emailform.email.value;
-    if(email.indexOf('@')<=0){
-        document.getElementById('errormsg').innerHTML="Invalid your Email Address";
-        return false;
-    }     
-    if ((email.charAt(email.length-4)!='.') && (email.charAt(email.length-3)!='.')) {
-        document.getElementById('errormsg').innerHTML="Invalid your Email Address";
-        return false;
-    }*/
-
-    /*function myFunction() {
-      var x = document.getElementById("menu");
-      if (x.style.display === "block") {
-        x.style.display = "none";
-      } else {
-        x.style.display = "block";
-      }
-    }*/
+    
 
  // Function to change webpage background color
  function changeBodyBg(color){
@@ -93,6 +79,8 @@ document.write(z);*/
 }
 
 
-function changeHeadingBg(color){
+function changeHeadingBg(color)
+{
     document.getElementById("heading").style.background = color;
 }
+  
